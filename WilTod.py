68576408b2fa72018bcd08e02f3a1fbff67f03b5
WilTodZ <96977564+WilTodZ@users.noreply.github.com>
2022-01-02 23:00:13 +0700

    Add files via upload

--- WilTod.py
+++ WilTod.py
@@ -2,6 +2,7 @@ import random
 import socket
 import threading
 
+os.system("clear")
 print       (" - - > William X Zen!!! < - - ")
 print       (" - - > Tolls By William!!!! < - - ")
 print       (" - - > Join Discord Indo Wolf!!! <- - ")                                   
